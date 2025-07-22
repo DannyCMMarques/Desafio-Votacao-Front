bbe2b419f856e1971f51bd2aa320917f498a5e13
Danielly Marques <danielly.marques@dbserver.com.br>
2025-07-22 16:40:22 -0300

    feat: criação de hook de obterSessaoPorPauta e alteração na pagina de pautasPageView

--- src/pages/Pautas/index.tsx
+++ src/pages/Pautas/index.tsx
@@ -1,7 +1,5 @@
-import { useCallback, useEffect, useState } from 'react';
 import { IoAddCircleOutline, IoNewspaper } from 'react-icons/io5';
-import { useNavigate } from 'react-router-dom';
-import { Bounce, toast, ToastContainer } from 'react-toastify';
+import { Bounce, ToastContainer } from 'react-toastify';
 import Cards from '../../components/cards/Cards';
 import ContainerComponent from '../../components/container';
 import FormularioPauta from '../../components/form/form_pauta';
@@ -9,115 +7,39 @@ import Loading from '../../components/loading';
 import Modal from '../../components/modal';
 import Paginador from '../../components/paginador';
 import VisualizarPauta from '../../components/visualizar-dados-pauta';
-import type { PautaPage, PautaResponseDTO } from '../../interfaces/interfaces/interfacePauta';
-import type { SessaoIniciadaResponseDTO } from '../../interfaces/interfaces/interfaceSessao';
-import usePautaService from '../../service/usePautaService';
-import useSessaoService from '../../service/useSessaoService';
-
-type ModalState = {
-  tipo: 'formulario' | 'resultado' | null;
-  id?: number | null;
-};
+import { useDeletarPautas } from '../../hooks/pautas/useDeletarPautas';
+import { useExibirPautasPaginadas } from '../../hooks/pautas/useExibirPautas';
+import { useObterSessaoPorPauta } from '../../hooks/sessao/useObterSessaoPorPauta';
+import { handleResumo } from '../../utils/helper/handleResumo';
+import { useSessaoNavigation } from '../../hooks/sessao/useSessaoNavigation';
+import { useModalCard } from '../../hooks/useModalCard';
 
 const PautasPageView = () => {
-  const pautaService = usePautaService();
-  const sessaoService = useSessaoService();
-  const navigate = useNavigate();
+  const { navegarParaSessao } = useSessaoNavigation();
 
-  const [pautas, setPautas] = useState<PautaResponseDTO[]>([]);
-  const [pagina, setPagina] = useState<number>(1);
-  const [totalPages, setTotalPages] = useState<number>(0);
-  const [isLoading, setIsLoading] = useState(false);
-  const [modal, setModal] = useState<ModalState>({ tipo: null, id: null });
-  const [sessaoDaPauta, setSessaoDaPauta] = useState<SessaoIniciadaResponseDTO | undefined>();
-  const [totalItens, setTotalItens] = useState<number>(0);
+  const { modal, abrirFormulario, abrirResultado, fecharModal } = useModalCard();
 
-  const size = 10;
+  const { exibirPautas, setPagina, totalPages, totalItens, pautas, isLoading, pagina } =
+    useExibirPautasPaginadas();
 
-  const obterIdSessao = async (idPauta: number): Promise<number | undefined> => {
-    try {
-      const response = await sessaoService.listarSessao(1, 10, 'id', 'desc', idPauta);
-      const sessao = response.content?.[0] as SessaoIniciadaResponseDTO;
-      setSessaoDaPauta(sessao);
-      if (sessao?.id !== undefined) {
-        return sessao.id;
-      }
-    } catch (err) {
-      console.error('Erro ao obter sessão', err);
-    }
-  };
+  const { onDelete } = useDeletarPautas({ exibirPautas, pagina });
 
-  const exibirPautas = useCallback(
-    async (page = 1) => {
-      setIsLoading(true);
-      try {
-        const response: PautaPage = await pautaService.listarPauta(page, size);
-        setPautas(response?.content);
-        setTotalItens(response?.totalElements);
-        setTotalPages(response.totalPages);
-      } catch (err) {
-        console.error(err, ' erro ao buscar pauta');
-      } finally {
-        setIsLoading(false);
-      }
-    },
-    [pautaService],
-  );
-
-  useEffect(() => {
-    exibirPautas(pagina);
-  }, [pagina, totalItens]);
-
-  const abrirModal = async (tipo: 'formulario' | 'resultado', id?: number) => {
-    if (tipo === 'resultado' && id !== undefined) {
-      await obterIdSessao(id);
-    }
-
-    setModal({ tipo, id: id ?? null });
-  };
-
-  const fecharModal = () => {
-    setModal({ tipo: null, id: null });
-  };
-
-  const handleResumo = (texto: string) => {
-    const limite = 230;
-    return texto.length <= limite ? texto : texto.slice(0, limite - 3).trim() + '...';
-  };
-
-  const onDelete = async (id: number) => {
-    try {
-      await pautaService.deletarPauta(id);
-      toast.success('Pauta excluída com sucesso');
-      exibirPautas(pagina);
-    } catch (err: unknown) {
-      toast.error(err?.response.data.message);
-    }
-  };
-
-  const handleNavigationSessao = async (idPauta: number) => {
-    const id = await obterIdSessao(idPauta);
-    if (id !== undefined) {
-      navigate(`/sessao/${id}`);
-    } else {
-      toast.error('Sessão não encontrada');
-    }
-  };
+  const { obterIdSessaoPorPauta, sessaoDaPauta } = useObterSessaoPorPauta();
 
   return (
     <main className="sm:px-0 lg:px-8 py-8 min-h-screen">
       <ToastContainer position="top-right" autoClose={5000} theme="colored" transition={Bounce} />
 
-      {modal.tipo === 'resultado' && modal.id != null && sessaoDaPauta !== undefined && (
+      {modal?.tipo === 'resultado' && modal.id != null && sessaoDaPauta && (
         <Modal onFechar={fecharModal} tamanho="xl">
           <VisualizarPauta id={modal.id} sessaoDaPauta={sessaoDaPauta} />
         </Modal>
       )}
 
-      {modal.tipo === 'formulario' && (
+      {modal?.tipo === 'formulario' && (
         <Modal onFechar={fecharModal} tamanho="md">
           <FormularioPauta
-            id={modal.id ?? undefined}
+            id={modal.id}
             handleClose={fecharModal}
             onSucesso={() => {
               exibirPautas(pagina);
@@ -134,7 +56,7 @@ const PautasPageView = () => {
         </div>
 
         <button
-          onClick={() => abrirModal('formulario')}
+          onClick={() => abrirFormulario()}
           className="inline-flex items-center gap-2 px-4 py-2 bg-indigo-600 text-white text-sm font-semibold rounded-lg shadow hover:bg-indigo-700 transition"
         >
           <IoAddCircleOutline className="text-lg" />
@@ -155,10 +77,12 @@ const PautasPageView = () => {
                 icon={<IoNewspaper />}
                 status={pauta.status}
                 id={pauta.id}
-                onEditar={(id) => abrirModal('formulario', id)}
+                onEditar={() => abrirFormulario(pauta.id)}
                 onExcluir={onDelete}
-                onVerResultados={(id) => abrirModal('resultado', id)}
-                onParticiparSessao={() => handleNavigationSessao(pauta.id)}
+                onVerResultados={() => {
+                  obterIdSessaoPorPauta(pauta.id).then(() => abrirResultado(pauta.id));
+                }}
+                onParticiparSessao={() => navegarParaSessao(pauta.id)}
               />
             ))}
           </div>
